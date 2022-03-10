a348e40d9f0a7b5d176ccaefb93e0763bab98e97
Ahmad Haydar Alkaf <78914402+ahmadhaydar@users.noreply.github.com>
2022-03-10 20:08:01 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const http = require('http');
 var server = http.createServer(app);
 require('dotenv').config({ path: './.env' });
 const mongoose = require('./db/connectDB');
-const port = process.env.PORT;
+const port = 8080;
 server.listen(port, function () {
     console.log("Server running at port " + port);
 });
