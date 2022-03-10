15808f5a5ae80e4f2f668c6bf9c3d5f915598247
Ahmad Haydar Alkaf <78914402+ahmadhaydar@users.noreply.github.com>
2022-03-10 20:08:45 +0700

    Update connectDB.js

--- src/db/connectDB.js
+++ src/db/connectDB.js
@@ -1,5 +1,5 @@
 require('dotenv').config({ path: './.env' });
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect("mongodb+srv://ahmadhaydar:NcrSfuSgAWw3MXrB@oauth2assignment.9kevr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
